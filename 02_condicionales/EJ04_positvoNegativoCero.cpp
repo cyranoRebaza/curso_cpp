89ae8d5392c6dec6a87ac7c4965a136d2dabe8de
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2024-09-03 13:59:00 -0300

    FEAT(.cpp): se resolvio ejercicio 04

--- 02_condicionales/EJ04_positvoNegativoCero.cpp
+++ 02_condicionales/EJ04_positvoNegativoCero.cpp
@@ -1,7 +1,8 @@
 
 /*
-    EJERCICIO O1:
-
+    EJERCICIO 04:
+    Hacer un programa para ingresar un número y luego se emita un cartel por pantalla “Positivo” si el número es
+    mayor a cero, “Negativo” si el número es menor a cero o “Cero” si el número es igual a cero.
 
 */
 
@@ -11,7 +12,26 @@ using namespace std;
 
 int main(){
 
-    cout << "BIENVENIDOS-HORA DE PRACTICAR" << endl;
+    // declaro las variables
+    int numero;
+
+    // pido el numero
+    cout << "Ingrese el numero: ";
+    cin >> numero;
+
+    // pregunto
+    if(numero > 0){
+        cout << "POSITIVO" << endl;
+    }
+    else{
+        if(numero < 0){
+            cout << "NEGATIVO" << endl;
+        }
+        else{
+            cout << "CERO" << endl;
+        }
+    }
+
 
     system("pause");
     return 0;
