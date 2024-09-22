53a92076ee56e68e181d0cd76c43a2ca9ce86e83
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2024-09-22 16:59:51 -0300

    FEAT(.cpp): se busco el numero maximo de una lista de numeros

---
+++ 04_ciclo_while/EJ06_maximoPosicion.cpp
@@ -0,0 +1,51 @@
+
+/*
+    EJERCICIO 06:
+    Hacer un programa que solicite una lista de números que corta cuando se
+    ingresa un cero y luego mostrar por pantalla el máximo de ellos y la posición
+    en la que fue ingresado.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // declaro las variables
+    int numero;
+    int numeroMaximo;
+    bool banderaMaximo = false;
+
+    // pido numero
+    cout << "Ingrese numero: ";
+    cin >> numero;
+
+
+    //carga numeros --> cortar con cero
+    while(numero != 0){
+
+        //pregunto si hay un maximo --> bandera
+        if(banderaMaximo == false){
+            numeroMaximo = numero;
+            banderaMaximo = true;
+        }
+        else{
+            if(numero > numeroMaximo){
+                numeroMaximo = numero;
+            }
+        }
+
+
+        // pido nuevo numero
+        cout << "Ingrese numero: ";
+        cin >> numero;
+    }
+
+    // muestro el numero maximo
+    cout << "El numero maximo es: " << numeroMaximo << endl;
+
+    system("pause");
+    return 0;
+}
